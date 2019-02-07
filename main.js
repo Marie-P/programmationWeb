204a721b53a5a6f20ab1d71a17d0a1d1793d8d63
Marie-P <47229106+Marie-P@users.noreply.github.com>
2019-02-07 21:05:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,13 @@ var people = [];
 function main()
 {
 	console.log(faker.fake("{{name.lastName}}"));
+		}
+		people.push(person);
+	}
 }
 
+function randRange(min, max){
+	// Permet de générer des éléments aléatoires
+
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
